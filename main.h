e7a013643df11e768103d191ff21239105828806
Martha <ehidiamenmartha@yahoo.com>
2023-05-19 06:17:08 -0700

    Exits the shell

--- main.h
+++ main.h
@@ -9,15 +9,16 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+
 void execute_commands(char **arguments); /* executes command passed to it */
 void create_childProcess(char **arguments); /* ensures after execution of command the loop continues to infinity */
 char **get_tokens(char *input, ssize_t bytes_read, char **arguments); /* splits the string into arguments */
 char *handle_path(char *command); /* gets location of a particular command */
 
-
-
-
-
+/* Exits shell */
+void builtin_exit(char **arr); 
+int _atoi(char *s);
+void freed(char **arr);
 
 
 #endif
